3301703d83f3091d20bf8faa86235d5a4bf1bf35
Richard Laskey <me@rlaskey.org>
2017-07-03 14:59:16 -0400

    first commit: very minimal setup.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TARGET = 'build';
+
+module.exports = (env = {}) => new Object({
+	entry: {
+		index: './src/index.jsx'
+	},
+	output: {
+		filename: (() => {
+			if (env.production) return '[name].[chunkhash].js';
+			return '[name].js';
+		})(),
+		path: path.resolve(__dirname, TARGET)
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		// https://webpack.js.org/guides/caching/#get-filenames-from-webpack-compilation-stats
+		function() {
+			if ( ! env.production) return false;
+
+			this.plugin('done', function(stats) {
+				require('fs').writeFileSync(
+					path.join(__dirname, TARGET, 'chunks-manifest.json'),
+					JSON.stringify(stats.toJson()['assetsByChunkName']));
+			});
+		},
+		new HtmlWebpackPlugin({
+			title: 'from RML3'
+		})
+	]
+});
