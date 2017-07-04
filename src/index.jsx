a74b42662f368aba03f63d56cd97c2aa105920b5
Richard Laskey <me@rlaskey.org>
2017-07-04 11:30:20 -0400

    cleaner start, w/ basic CSS.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
+import css from './index.css';
+
+function Attribution(props) {
+	return (<p>
+		started off by <a href="https://github.com/rlaskey/start-react">
+			Richard Laskey
+		</a>.
+	</p>);
+}
 
 class App extends React.Component {
 	render () {
-		return <p>Hello React!</p>;
+		return (<main>
+			<h1>Your Words Here.</h1>
+			<Attribution />
+		</main>);
 	}
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App/>, document.getElementById('root'));
